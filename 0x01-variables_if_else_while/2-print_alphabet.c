fcf4dfaa36e634130868a1fd2a3566782f0ba75f
Gesh1 <sbu9617@gmail.com>
2023-08-14 12:08:16 -0700

    positive or negetive

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Main - Prints th alphabetic
+ * Return: Always (success)
+ */
+
+int main ()
+
+   {
+	  char c;
+
+	  for (c = 'a';c <= 'z';c++)
+	  {
+	  putchar(c);
+	 
+	  putchar('\n');}
+	  return 0;
+   }
