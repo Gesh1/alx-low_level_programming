062cc22f9f2f62d5361b6c08659a1c4cb6f348a8
Gesh1 <sbu9617@gmail.com>
2023-08-14 13:23:28 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,15 @@
  * Return: Always (success)
  */
 
-int main ()
+int main(void)
 
-   {
-	  char c;
+ {
+     char c;
 
-	  for (c = 'a';c <= 'z';c++)
-	  {
-	  putchar(c);
+   for (c = 'a'; c <= 'z'; c++)
+   
+  putchar(c);
 	 
-	  putchar('\n');}
-	  return 0;
-   }
+  putchar('\n');
+  return 0;
+  }
